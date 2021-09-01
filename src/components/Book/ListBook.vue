e366356cb250f767b52047d9485e6a97c5fe3477
Emanuel <emanuelX92@gmail.com>
2021-09-01 06:49:50 -0400

    components ListBooks

---
+++ src/components/Book/ListBook.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col text-left">
+        <h2>Listado de libros</h2>
+
+        <div class="col-md-12">
+          <b-table striped hover :items="books" :fields="fields"> </b-table>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+export default {
+    data(){
+        return {
+            fields:[
+                { key: 'title', label: 'Titulos'},
+                { key: 'decription',label:'Descripciòn'},
+                { key: 'year', label:'año'},
+                { key: ' front_page', label:'Portada'},
+                { key: 'action', label:'acciòn'},
+                
+            ],
+            books:[]
+        }
+    },
+    methods:{
+        getBooks(){
+            const path= 'http://localhost:8000/api/v1.0/book/'
+            axios.get(path).then((response) => {
+                this.books = response.data
+
+            })
+            .catch((error)=>{
+                console.log("Error")
+            })
+        }
+    },
+    created(){
+        this.getBooks()
+    }
+
+}
+</script>
+
+<style lang="css" scoped>
+</style>
--- src/main.js
+++ src/main.js
@@ -3,9 +3,16 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 Vue.config.productionTip = false
 
+Vue.use(BootstrapVue);
+// Optionally install the BootstrapVue icon components plugin
+Vue.use(IconsPlugin)
+
+// Import Bootstrap an BootstrapVue CSS files (order is important)
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
